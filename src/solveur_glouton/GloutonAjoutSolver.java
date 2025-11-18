379d232ba67aca8d83286471c1eb734d7fb99da4
ClaraBgr <clara.baigneres@icloud.com>
2025-11-18 22:04:08 +0100

    Nouvelle version avec comparateur

--- src/solveur_glouton/GloutonAjoutSolver.java
+++ src/solveur_glouton/GloutonAjoutSolver.java
@@ -1,5 +1,57 @@
 package solveur_glouton;
 
-public class GloutonAjoutSolver {
 
+/*import java.util.*;
+import sacADos.Objet; // ici j'ai importe la classe Objet du package SacADos (dans mes fiches de cours)
+public class GloutonAjoutSolver {
+	
+	public static void main () {
+		getDimension dim = new Dimension () ;
+		UtiliteTotale utilite = new UtiliteTotale () ;
+		CoutTotal cout = new CoutTotal ();
+		dim.getDimension();
+		utilite.getUtilite();
+		cout.getCout();
+	} // ici j'ai créé une fonction main pour récupérer un objet issu de la fonction getDimension, getUtilite et getCout (video 1)
+
+	private List[] S;
+
+	public void ajoutS(List[] S){
+		this.S = S ; 
+	}
 }
+*/
+import java.util.*;
+
+
+interface ObjetDansSacADos {
+	int getUtilite();
+	int[] getCouts();
+}
+
+interface SacADos {
+	int getDimension();
+	int[] getBudgets();
+	List<ObjetDansSacADos> getObjets();
+}
+
+
+public class GloutonAjoutSolver {
+	
+	public static List<ObjetDansSacADos> resolutionParMethodeGloutonne(SacADos sacADos) {
+		List<ObjetDansSacADos> result = List.of();
+		
+		// On trie les objets du sac a dos
+		List<ObjetDansSacADos> tousLesObjets = sacADos.getObjets();
+		SortByInterest comparatorByInterest = new SortByInterest();
+		tousLesObjets.sort(comparatorByInterest);
+		
+		
+		
+		
+		return result;
+	}
+	
+	
+}
+
--- src/solveur_glouton/SortByInterest.java
+++ src/solveur_glouton/SortByInterest.java
@@ -1,5 +1,35 @@
 package solveur_glouton;
 
-public class SortByInterest {
+import java.util.Comparator;
+
+
+public class SortByInterest implements Comparator<ObjetDansSacADos> {
+	
+	private int sommeDesCouts(ObjetDansSacADos o) {
+		int somme = 0;
+		for (int c : o.getCouts()) {
+			somme += c;
+		}
+		return somme;
+	}
+	
+	
+	private double interet(ObjetDansSacADos o) {
+		return ((double) o.getUtilite()) / ((double) sommeDesCouts(o));
+	}
+
+	
+	@Override
+	public int compare(ObjetDansSacADos o1, ObjetDansSacADos o2) {
+		double interet1 = interet(o1);
+		double interet2 = interet(o2);
+		if(interet1 < interet2) {
+			return -1;
+		} else if(interet1 == interet2) {
+			return 0;
+		} else {
+			return 1;
+		}
+	}
 
 }
