75ec1aeae35a26f05c492df50d90a72ac3439857
Fairy-maker <fairyb128@gmail.com>
2025-11-22 23:39:49 +0100

    Ajout de la classe main + finalisation des derniers classe de equipe

--- src/equipe/Expert.java
+++ src/equipe/Expert.java
@@ -1,74 +1,26 @@
 package equipe;
 
 import java.util.*;
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
- 
+
 public class Expert extends Personne{
-	private List <Secteur> secteur;
-	
+	private List <Secteur> secteur; //chaque expert est spécialisé dans une liste de secteurs que l'on a definit dans notre énumération secteur
+	private Random random = new Random();  //pour faire le processus stochastique
+
 	public Expert (String nom, String prenom, int age, List <Secteur> secteur) {
 		super(nom, prenom, age);
+		this.secteur = secteur; 
+	}
+
+	public List<Secteur> getSecteur() {
+		return secteur;
+	}
+
+	public void setSecteurs(List<Secteur> secteur) {
 		this.secteur = secteur;
 	}
 
 	public Projet proposer_projet () {
-		Secteur secteurchoisi = secteur;
-		String titre = titre;
-		String description = description; 
-		int benefice = benefice; 
-		Cout cout = new Cout();
-	return new Projet (titre, description,benefice,secteurchoisi, cout);
-=======
-
-public class Expert extends Personne{
-	private List <Secteur> secteurs; //chaque expert est spécialisé dans une liste de secteurs que l'on a definit dans notre énumération secteur
-    private Random random = new Random();  //pour faire le processus stochastique
-	
-	public Expert (String nom, String prenom, int age, List <Secteur> secteurs) {
-		super(nom, prenom, age);
-		this.secteurs = secteurs; 
-	}
-
-	public List<Secteur> getSecteurs() {
-		return secteurs;
-	}
-
-	public void setSecteurs(List<Secteur> secteurs) {
-		this.secteurs = secteurs;
-	}
-
-	public Projet proposer_projet () {
-		Secteur secteurchoisi = secteurs.get(random.nextInt(secteurs.size())); //Un expert est spécialisé dans un secteur aléatoire parmi ceux proposé par l'énoncé
-		String titre = "un projet dans le secteur " + secteurchoisi; // génère le titre du projet dans le secteur choisi
-		String description = "Projet dans le secteur " + secteurchoisi; //génère la description du projet dans le secteur choisi
-		Projet p = new Projet(titre, description, secteurchoisi); //créer/propose le projet
-		System.out.println("L'expert "+ getPrenom()+ " " + getNom()+ " propose " + p.getTitre()); //nous renvoie dans la console le projet proposé par tel ou tel expert
-		return p; //renvoie le projet
->>>>>>> Stashed changes
-	}
-}
-=======
-
-public class Expert extends Personne{
-	private List <Secteur> secteurs; //chaque expert est spécialisé dans une liste de secteurs que l'on a definit dans notre énumération secteur
-    private Random random = new Random();  //pour faire le processus stochastique
-	
-	public Expert (String nom, String prenom, int age, List <Secteur> secteurs) {
-		super(nom, prenom, age);
-		this.secteurs = secteurs; 
-	}
-
-	public List<Secteur> getSecteurs() {
-		return secteurs;
-	}
-
-	public void setSecteurs(List<Secteur> secteurs) {
-		this.secteurs = secteurs;
-	}
-
-	public Projet proposer_projet () {
-		Secteur secteurchoisi = secteurs.get(random.nextInt(secteurs.size())); //Un expert est spécialisé dans un secteur aléatoire parmi ceux proposé par l'énoncé
+		Secteur secteurchoisi = secteur.get(random.nextInt(secteur.size())); //Un expert est spécialisé dans un secteur aléatoire parmi ceux proposé par l'énoncé
 		String titre = "un projet dans le secteur " + secteurchoisi; // génère le titre du projet dans le secteur choisi
 		String description = "Projet dans le secteur " + secteurchoisi; //génère la description du projet dans le secteur choisi
 		Projet p = new Projet(titre, description, secteurchoisi); //créer/propose le projet
@@ -76,4 +28,3 @@ public class Expert extends Personne{
 		return p; //renvoie le projet
 	}
 }
->>>>>>> Stashed changes
